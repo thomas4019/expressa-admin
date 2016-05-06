04e645324a06e0acc1c9b2d1b11b1d364e60fae2
Thomas Hansen <th4019@gmail.com>
2016-05-06 11:59:36 -0600

    switched to using webpack

--- src/index.js
+++ src/index.js
@@ -11,13 +11,33 @@ var browserHistory = require('react-router').browserHistory
 var hashHistory = require('react-router').hashHistory
 var config = require('./config')
 
-config.setAPIURL('http://localhost:3000/api/')
+config.setAPIURL(window.apiURL ? window.apiURL : '/api/')
 
 //var jsonschemaeditor = require('json-schema-editor')
 //var JSONEditor = require('json-editor');
 
 require("expose?React!react");
 require("expose?ReactDOM!react-dom");
+require("expose?config!./config");
+
+require('../styles.css')
+
+require('expose?$!expose?jQuery!jquery');
+
+require("bootstrap-webpack");
+
+require("font-awesome-webpack");
+
+require("toastr");
+//require("file?name=toastr.css!./node_modules/toastr/toastr.less");
+
+require('json-editor')
+require("expose?!json-editor");
+require("exports?JSONEditor!json-editor")
+require("expose?JSONScheamEditor!json-schema-editor");
+
+//require("json-schema-editor!./src/json-editor-plugin.js");
+
 
 window.config = config;
 
@@ -131,5 +151,83 @@ ReactDOM.render(
 		</Route>
 	</Router>
 , document.getElementById('app'))
+
+if (JSONEditor) {
+  JSONEditor.defaults.resolvers.unshift(function(schema) {
+    if (schema.type === "object" && schema.format === "schema") {
+      return "schema";
+    }
+
+    // If no valid editor is returned, the next resolver function will be used
+  });
+
+  JSONEditor.defaults.editors.schema = JSONEditor.AbstractEditor.extend({
+    setValue: function(value,initial) {
+      this.value = value;
+    	this.schemaeditor.setValue(value)
+      this.onChange();
+    },
+    getValue: function() {
+    	if (typeof this.schemaeditor != 'undefined')
+    		return this.schemaeditor.getValue();
+     	else
+     		return {}
+    },
+    register: function() {
+      this._super();
+      if(!this.input) return;
+      this.input.setAttribute('name', this.formname);
+    },
+    unregister: function() {
+      this._super();
+      if(!this.input) return;
+      this.input.removeAttribute('name');
+    },
+    getNumColumns: function() {
+      return 12;
+    },
+    build: function() {
+      var self = this;
+      if(!this.options.compact) {
+        this.label = this.header = this.theme.getFormInputLabel(this.getTitle());
+      }
+      if(this.schema.description) this.description = this.theme.getFormInputDescription(this.schema.description);
+      if(this.options.compact) this.container.className += ' compact';
+
+      this.input = document.createElement('div')
+      this.control = this.theme.getFormControl(this.label, this.input, this.description);
+
+      if(this.schema.readOnly || this.schema.readonly) {
+        this.always_disabled = true;
+        this.input.disabled = true;
+      }
+
+      this.container.appendChild(this.control);
+
+      this.schemaeditor = new JSONSchemaEditor(this.input, {
+      	startval: {}
+      });
+      this.schemaeditor.on('change', function() {
+      	self.onChange(true);
+      });
+    },
+    enable: function() {
+      if(!this.always_disabled) {
+        this.input.disabled = false;
+      }
+      this._super();
+    },
+    disable: function() {
+      this.input.disabled = true;
+      this._super();
+    },
+    destroy: function() {
+      if(this.label && this.label.parentNode) this.label.parentNode.removeChild(this.label);
+      if(this.description && this.description.parentNode) this.description.parentNode.removeChild(this.description);
+      if(this.input && this.input.parentNode) this.input.parentNode.removeChild(this.input);
+      this._super();
+    }
+  });
+}
 
 module.exports = Home;
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,12 @@ module.exports = {
             query: {
                 presets: ['es2015', 'react']
             }
-        }],
+        },{ test: /\.css$/, loader: "style-loader!css-loader" },
+        {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+],
     },
     devServer: {
         historyApiFallback: {
